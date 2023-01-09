22a574b909d8ba5b15829220e6003b0f83ebf97a
Mike <vinay9955@gmail.com>
2023-01-09 16:36:14 +0400

    formating

--- src/Manager/OhmApiManager.php
+++ src/Manager/OhmApiManager.php
@@ -11,37 +11,66 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-class OhmApiManager  extends BaseHttpCalls
+class OhmApiManager extends BaseHttpCalls
 {
-    /**
-     * @var username
-     */
-    private  $username;
+    const RELATIVE_URL = '/api/';
 
     /*
      * @var password
      */
+    /**
+     * @var username
+     */
+    private $username;
     private $password;
-
-
-    const RELATIVE_URL = '/api/';
     /**
      * @var null
      */
     private $token;
 
+    public function __construct(ContainerBagInterface $containerBag, SessionInterface $session, TraceJourneyHandler $logger, EntityManagerInterface $em = null)
+    {
+        parent::__construct($containerBag, $session, $logger, $em);
+        $this->username = $this->containerBag->get('ohm.username');
+        $this->password = $this->containerBag->get('ohm.password');
+        $this->token = null;
+
+    }
+
+    /**
+     * @return void
+     */
+    public function getToken()
+    {
+
+        $this->init();
+        $response = $this->send(['username' => $this->username, 'password' => $this->password, 'action_path' => 'login_check']);
+        if ($response['code'] == Response::HTTP_OK) {
+            $this->token = $response['body']['token'];
+        } else {
+
+            throw new Exception('Une erreur est survenue lors de la recuperation du token');
+        }
+
+        return $this->token;
+    }
+
+    public function init()
+    {
+        $this->withJsonHeaders();
+        return $this;
+    }
 
     public function send($params = [])
     {
-        $uri = $this->containerBag->get('ohm.base_url')  . self::RELATIVE_URL;
-        if(array_key_exists('action_path', $params) && isset($params['action_path']))
-        {
+        $uri = $this->containerBag->get('ohm.base_url') . self::RELATIVE_URL;
+        if (array_key_exists('action_path', $params) && isset($params['action_path'])) {
             $uri .= $params['action_path'];
         }
         try {
             $startTime = microtime(true);
             $response = $this->httpClient->request(Request::METHOD_POST, $uri, [
-                'body' => json_encode(!empty($params) ?$params: null),
+                'body' => json_encode(!empty($params) ? $params : null),
                 'headers' => $this->headers,
                 'allow_redirects' => false
             ]);
@@ -58,40 +87,5 @@ class OhmApiManager  extends BaseHttpCalls
                 'body' => $exception->getMessage(),
             ];
         }
-    }
-
-    public function init()
-    {
-        $this->withJsonHeaders();
-        return $this;
-    }
-
-    public function __construct(ContainerBagInterface $containerBag, SessionInterface $session, TraceJourneyHandler $logger, EntityManagerInterface $em = null)
-    {
-        parent::__construct($containerBag, $session, $logger, $em);
-        $this->username  = $this->containerBag->get('ohm.username');
-        $this->password  = $this->containerBag->get('ohm.password');
-        $this->token = null;
-
-    }
-
-    /**
-     * @return void
-     */
-    public function getToken()
-    {
-
-        $this->init();
-        $response = $this->send(['username'=> $this->username, 'password'=> $this->password, 'action_path' => 'login_check']);
-        if($response['code'] ==Response::HTTP_OK)
-        {
-            $this->token = $response['body']['token'];
-        }
-        else{
-
-            throw new Exception('Une erreur est survenue lors de la recuperation du token');
-        }
-
-        return $this->token;
     }
 }
